f72c5a8555385e56b4999c5ccb4b08b6970c4ccb
azan-personal <112457939+azanra@users.noreply.github.com>
2026-08-17 08:58:30 +0700

    feat: add utils to capitalize first character

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 const utils = (() => {
-  const capitalize = (input: string) => {};
+  const capitalize = (input: string) => {
+    return input[0].toUpperCase() + input.slice(1).toLowerCase();
+  };
 
   return { capitalize };
 })();
