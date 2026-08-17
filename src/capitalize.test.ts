356c45afc90f9bfdbc46aafc0cb066f4c81cd139
azan-personal <112457939+azanra@users.noreply.github.com>
2026-08-17 08:42:57 +0700

    test: add test case for capitalize utils

--- src/capitalize.test.ts
+++ src/capitalize.test.ts
@@ -2,4 +2,10 @@ import { test, expect } from "@jest/globals";
 
 test("capitalize first character", () => {
   expect(utils.capitalize("hello world")).toBe("Hello world");
+
+  expect(utils.capitalize("John Doe")).toBe("John doe");
+
+  expect(utils.capitalize("This Is a Longer String")).toBe(
+    "This is a longer string",
+  );
 });
