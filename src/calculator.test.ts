299a6d087ca4ea3d32f0a721a810e33747efa40c
azan-personal <112457939+azanra@users.noreply.github.com>
2026-08-18 07:18:00 +0700

    test: expect exact number accuracy for addition

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -4,15 +4,15 @@ import utils from ".";
 describe("calculator", () => {
   describe("addition", () => {
     test("addition 1 + 2 to be 3", () => {
-      expect(utils.calculation.add(1, 2)).toBe(3);
+      expect(utils.calculator.add(1, 2)).toBe(3);
     });
 
     test("addition 0.1 + 0.2 to be 0.3", () => {
-      expect(utils.calculation.add(0.1, 0.2)).toBeCloseTo(0.3);
+      expect(utils.calculator.add(0.1, 0.2)).toBe(0.3);
     });
 
     test("addition -0.69 to 84.21 to be 83.25", () => {
-      expect(utils.calculatiion.add(-0.69, 84.21)).toBe(83.25);
+      expect(utils.calculator.add(-0.69, 84.21)).toBe(83.52);
     });
   });
 });
