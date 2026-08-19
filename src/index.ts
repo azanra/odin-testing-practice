254bdcb93d11e36e6db868eb474df013cc410a1c
azan-personal <112457939+azanra@users.noreply.github.com>
2026-08-19 07:30:40 +0700

    refactor: add utils to fixed decimal point result

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,21 @@ const utils = (() => {
     return input.split("").reverse().join("");
   };
 
+  const toFixedPoint = (result: number, fixedPoint = 12) =>
+    parseFloat(result.toFixed(fixedPoint));
+
   const calculator = {
     add: function (firstNumber: number, secondNumber: number) {
-      return parseFloat((firstNumber + secondNumber).toFixed(12));
+      return toFixedPoint(firstNumber + secondNumber);
     },
     subtract: function (firstNumber: number, secondNumber: number) {
-      return parseFloat((firstNumber - secondNumber).toFixed(12));
+      return toFixedPoint(firstNumber - secondNumber);
     },
     division: function (firstNumber: number, secondNumber: number) {
-      return parseFloat((firstNumber / secondNumber).toFixed(10));
+      return toFixedPoint(firstNumber / secondNumber, 10);
     },
     multiply: function (firstNumber: number, secondNumber: number) {
-      return parseFloat((firstNumber * secondNumber).toFixed(12));
+      return toFixedPoint(firstNumber * secondNumber);
     },
   };
 
